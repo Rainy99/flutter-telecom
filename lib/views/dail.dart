6641dd92085f265c0e8e8369aa017c96cda8f94e
devzhou <y@devzhou.cn>
2019-07-30 20:54:42 +0800

    Change layout

--- lib/views/dail.dart
+++ lib/views/dail.dart
@@ -10,77 +10,104 @@ class Dail extends StatelessWidget {
 }
 
 class Dialpad extends StatefulWidget {
+  @override
   _DialpadState createState() => _DialpadState();
 }
 
 class _DialpadState extends State<Dialpad> {
   var phoneNumberList = new List<int>();
-  var phoneNumber = "0";
+  var phoneNumber = "";
 
   _setPhoneNumber(int value) {
     setState(() {
       phoneNumberList.add(value);
-      phoneNumber = value.toString();
+      phoneNumber = "1111";
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    phoneNumber = "0";
   }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
+      alignment: Alignment.bottomCenter,
       decoration: BoxDecoration(color: Colors.white),
-      child: Center(
+      child: Container(
+          height: 500,
           child: Column(
-        children: <Widget>[
-          Text(phoneNumber),
-          Column(
             children: <Widget>[
-              Row(
+              Text(phoneNumber),
+              Column(
                 children: <Widget>[
-                  Expanded(
-                      child: FlatButton(
-                          child: Text("1"), onPressed: _setPhoneNumber(1))),
-                  Expanded(
-                      child: FlatButton(
-                          child: Text("2"), onPressed: _setPhoneNumber(2))),
-                  Expanded(
-                      child: FlatButton(
-                          child: Text("3"), onPressed: _setPhoneNumber(3)))
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                          child: FlatButton(
+                              padding: EdgeInsets.only(top: 40, bottom: 40),
+                              child: Text("1"),
+                              onPressed: _setPhoneNumber(1))),
+                      Expanded(
+                          child: FlatButton(
+                              padding: EdgeInsets.only(top: 40, bottom: 40),
+                              child: Text("2"),
+                              onPressed: _setPhoneNumber(2))),
+                      Expanded(
+                          child: FlatButton(
+                              padding: EdgeInsets.only(top: 40, bottom: 40),
+                              child: Text("3"),
+                              onPressed: _setPhoneNumber(3)))
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                          child: FlatButton(
+                              padding: EdgeInsets.only(top: 40, bottom: 40),
+                              child: Text("4"),
+                              onPressed: _setPhoneNumber(4))),
+                      Expanded(
+                          child: FlatButton(
+                              padding: EdgeInsets.only(top: 40, bottom: 40),
+                              child: Text("5"),
+                              onPressed: _setPhoneNumber(5))),
+                      Expanded(
+                          child: FlatButton(
+                              padding: EdgeInsets.only(top: 40, bottom: 40),
+                              child: Text("6"),
+                              onPressed: _setPhoneNumber(6)))
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                          child: FlatButton(
+                              padding: EdgeInsets.only(top: 40, bottom: 40),
+                              child: Text("7"),
+                              onPressed: _setPhoneNumber(7))),
+                      Expanded(
+                          child: FlatButton(
+                              padding: EdgeInsets.only(top: 40, bottom: 40),
+                              child: Text("8"),
+                              onPressed: _setPhoneNumber(8))),
+                      Expanded(
+                          child: FlatButton(
+                              padding: EdgeInsets.only(top: 40, bottom: 40),
+                              child: Text("9"),
+                              onPressed: _setPhoneNumber(9)))
+                    ],
+                  )
                 ],
               ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                      child: FlatButton(
-                          child: Text("4"), onPressed: _setPhoneNumber(4))),
-                  Expanded(
-                      child: FlatButton(
-                          child: Text("5"), onPressed: _setPhoneNumber(5))),
-                  Expanded(
-                      child: FlatButton(
-                          child: Text("6"), onPressed: _setPhoneNumber(6)))
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                      child: FlatButton(
-                          child: Text("7"), onPressed: _setPhoneNumber(7))),
-                  Expanded(
-                      child: FlatButton(
-                          child: Text("8"), onPressed: _setPhoneNumber(8))),
-                  Expanded(
-                      child: FlatButton(
-                          child: Text("9"), onPressed: _setPhoneNumber(9)))
-                ],
+              FloatingActionButton(
+                child: Icon(Icons.call),
               )
             ],
-          ),
-          FloatingActionButton(
-            child: Icon(Icons.call),
-          )
-        ],
-      )),
+          )),
     );
   }
 }
